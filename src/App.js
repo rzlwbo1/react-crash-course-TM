2975291788601a680bc1aabe13629c7f04dadf12
rzlwbo1 <rizalwb2@gmail.com>
2022-09-02 19:51:45 +0700

    Global state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import React from 'react'
+import {React, useState} from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 import './style.css'
 
 
 function App() {
+
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Lorem Ipsum Dolor",
+      day: "Feb 5th at 2:30pm",
+      reminder: true
+    },
+    {
+      id: 2,
+      text: "Lorem Ipsum Dolor Sit ",
+      day: "Feb 6th at 2:30pm",
+      reminder: true
+    },
+    {
+      id: 3,
+      text: "Lorem Ipsum Dolor Sit Amet",
+      day: "Feb 7th at 2:30pm",
+      reminder: false
+    }
+  ])
+
   return (
     <div className='container'>
       <Header />
-      <Tasks />
+      <Tasks tasks={tasks}/>
     </div>
   )
 }
